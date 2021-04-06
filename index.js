64169fb3951888dc589819c67a837b5b05e329f8
Neil <zooming.in@hotmail.com>
2021-04-06 21:25:39 +1000

    updates

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ class Bird {
         this.sex = sex;
     }
     speak() {
-        if (this.sex === "male") {
+    if (this.sex === "male") {
         return `It's me! ${this.name}, the parrot!`
     } else {
         return `${this.name} says squawk!`
